fcf42323a9b9077811cbc1523302e1a848eacc85
Monika <molinskamonika+github@gmail.com>
2019-04-16 14:20:50 +0200

    extract single album to separate component

--- src/components/Playlists.js
+++ src/components/Playlists.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import coverPlaceholder from '../assets/headphones.png';
+import SingleAlbum from './SingleAlbum';
 
 class Playlists extends Component {
 
@@ -59,38 +59,12 @@ class Playlists extends Component {
 
         <h4 className="albums__heading">Albums</h4>
         <div className="albums__wrapper">
-          {albums.length !== 0 && albums.map(((singleAlbum, index) => 
-              <ul className="albums__single-album single-album__list">
-              <img 
-                className="album__img" 
-                src={(!singleAlbum.images || singleAlbum.images.length == 0) 
-                ? coverPlaceholder 
-                : singleAlbum.images[0].url}
-                alt={singleAlbum.name}>
-              </img>
-                <li 
-                  className="single__album__item single__album-name">
-                  {singleAlbum.name}
-                </li>
-                <li 
-                  className="single__album__item single__album-artist">
-                  {singleAlbum.artists[0].name}
-                </li>
-                <li
-                  className="single__album__item">
-                  {singleAlbum.release_date}
-                </li>
-                <li
-                  className="single__album__item">
-                  {singleAlbum.total_tracks} tracks
-                </li>
-                <button 
-                  className="btn btn--album-details"
-                  onClick={()=>showAlbumDetails(singleAlbum.id)}>
-                  album details
-                </button>
-              </ul>
-            ))}
+          {albums.length !== 0 && albums.map(((singleAlbum) => (
+              <SingleAlbum 
+                singleAlbum={singleAlbum}
+                showAlbumDetails={showAlbumDetails}
+              />
+          )))}
         </div>
       </React.Fragment> 
       )
--- src/components/SingleAlbum.js
+++ src/components/SingleAlbum.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import coverPlaceholder from '../assets/headphones.png';
 
-export default function SingleAlbum() {
-  return (
-    <React.Fragment>
-      
-    </React.Fragment>
-  )
+class SingleAlbum extends Component {
+  render () {
+
+    const { singleAlbum, showAlbumDetails } = this.props;
+
+    return (
+      <React.Fragment>
+        <ul className="albums__single-album single-album__list">
+          <img 
+            className="album__img" 
+            src={(!singleAlbum.images || singleAlbum.images.length === 0) 
+            ? coverPlaceholder 
+            : singleAlbum.images[0].url}
+            alt={singleAlbum.name}>
+          </img>
+            <li 
+              className="single__album__item single__album-name">
+              {singleAlbum.name}
+            </li>
+            <li 
+              className="single__album__item single__album-artist">
+              {singleAlbum.artists[0].name}
+            </li>
+            <li
+              className="single__album__item">
+              {singleAlbum.release_date}
+            </li>
+            <li
+              className="single__album__item">
+              {singleAlbum.total_tracks} tracks
+            </li>
+            <button 
+              className="btn btn--album-details"
+              onClick={()=>showAlbumDetails(singleAlbum.id)}>
+              album details
+            </button>
+        </ul>  
+      </React.Fragment>
+    )
+  }
 }
+
+export default SingleAlbum;
