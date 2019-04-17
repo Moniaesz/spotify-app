74c8084bf0e40a7d7535d048bf463627b91f2056
Monika <molinskamonika+github@gmail.com>
2019-04-17 18:45:37 +0200

    reset albumsDetails state before each fetch

--- src/App.js
+++ src/App.js
@@ -63,7 +63,8 @@ class App extends Component {
   
     this.setState({
       query,
-      error: ''
+      error: '',
+      albumDetails: null
     })
 
     fetch(`https://api.spotify.com/v1/search?q=${query}&type=album`, {
