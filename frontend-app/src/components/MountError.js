13e9fcdbf72414e294813a3fa9d7142bec737a2b
Monika <molinskamonika+github@gmail.com>
2019-04-20 13:10:37 +0200

    add link to port 8000

--- frontend-app/src/components/MountError.js
+++ frontend-app/src/components/MountError.js
@@ -5,7 +5,8 @@ export default function MountError() {
     <div className="mount-error">
       <h4 className="mount-error__heading">UPS!</h4>
       <p className="mount-error__par">You probably didn't authorize with valid access token or your token expired.</p>
-      <p className="mount-error__par">Make sure that both react development server and backend-app server are running and go to localhost:8888/login/</p>
+      <p className="mount-error__par">Make sure that both react development server and backend-app server are running and go to <a className="login__link" href="http://localhost:8888/login/">localhost:8888/login/</a>
+      </p>
     </div>
   )
 }
